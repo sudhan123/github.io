67cbf8b388b49c5ab96997771a5af0eea05e1bf1
sudhan <sudhandc7@gmail.com>
2022-08-18 10:48:08 +0900

    deploying latest code to develop

--- readme.md
+++
@@ -1,6 +0,0 @@
-## sudhan123.github.io/
-```
-$npm i
-$npm run serve
-$npm build  
-```
--- src/components/SocialIcons.vue
+++
@@ -1,8 +0,0 @@
-<template>
-    <p class="colorlib-social-icons">
-<!--        <a href="#"><i class="icon-facebook4"></i></a>-->
-<!--        <a href="#"><i class="icon-twitter3"></i></a>-->
-        <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/sudhan-dc-0b0566126/" class="pa-2"><i class="icon-linkedin4"></i></a>
-        <a target="_blank" rel="noopener noreferrer" href="https://sudhan123.github.io"><i class="icon-dribbble2"></i></a>
-    </p>
-</template>
